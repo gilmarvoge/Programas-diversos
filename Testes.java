52fce4bc01811294d5b21972330ce91f8d40b569
gilmarvoge <gilmarvoge@gmail.com>
2018-03-07 14:47:42 -0300

    Update Testes.java

--- Testes.java
+++ Testes.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package testes;
+
 
 import java.util.Scanner;
 
@@ -11,7 +6,7 @@ import java.util.Scanner;
  *
  * @author Gilmar Vogel
  */
-public class Testes {
+public class fibonacci {
 
     /**
      * @param args the command line arguments
